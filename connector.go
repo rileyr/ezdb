feeb4ea1de852e6425411ec292621b1cad56d788
Riley Rook <riley@coinlist.co>
2025-10-21 17:19:05 -0500

    throw error if env var is not actually present

--- connector.go
+++ connector.go
@@ -1,6 +1,10 @@
 package ezdb
 
-import "os"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 // Connector defines the interface for obtaining database connection information.
 type Connector interface {
@@ -28,12 +32,59 @@ type EnvConnector struct {
 }
 
 func (e EnvConnector) Data() (ConnectionData, error) {
+	// Be explicit, so we dont use other(default) configs in shared envs:
+
+	// Validate required keys are configured
+	var missingKeys []string
+	if e.UserKey == "" {
+		missingKeys = append(missingKeys, "UserKey")
+	}
+	if e.HostKey == "" {
+		missingKeys = append(missingKeys, "HostKey")
+	}
+	if e.DbKey == "" {
+		missingKeys = append(missingKeys, "DbKey")
+	}
+	if e.PortKey == "" {
+		missingKeys = append(missingKeys, "PortKey")
+	}
+
+	if len(missingKeys) > 0 {
+		return ConnectionData{}, fmt.Errorf("EnvConnector missing required key configuration: %s", strings.Join(missingKeys, ", "))
+	}
+
+	// Get values from environment
+	user := os.Getenv(e.UserKey)
+	host := os.Getenv(e.HostKey)
+	db := os.Getenv(e.DbKey)
+	port := os.Getenv(e.PortKey)
+
+	// Validate required environment variables are set
+	var missingEnvVars []string
+	if user == "" {
+		missingEnvVars = append(missingEnvVars, e.UserKey)
+	}
+	if host == "" {
+		missingEnvVars = append(missingEnvVars, e.HostKey)
+	}
+	if db == "" {
+		missingEnvVars = append(missingEnvVars, e.DbKey)
+	}
+	if port == "" {
+		missingEnvVars = append(missingEnvVars, e.PortKey)
+	}
+
+	if len(missingEnvVars) > 0 {
+		return ConnectionData{}, fmt.Errorf("required environment variables not set: %s", strings.Join(missingEnvVars, ", "))
+	}
+
+	// Optional fields (Password might use peer/trust auth, SSL is optional)
 	return ConnectionData{
-		User:        os.Getenv(e.UserKey),
+		User:        user,
 		Password:    os.Getenv(e.PasswordKey),
-		Host:        os.Getenv(e.HostKey),
-		Db:          os.Getenv(e.DbKey),
-		Port:        os.Getenv(e.PortKey),
+		Host:        host,
+		Db:          db,
+		Port:        port,
 		SslMode:     os.Getenv(e.SslKey),
 		SslRootCert: os.Getenv(e.SslCertKey),
 	}, nil
